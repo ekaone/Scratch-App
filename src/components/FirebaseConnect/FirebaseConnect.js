9d73023fb3fc6a902e2ccdc26e449faf9a18c226
ekaone <ekaone3033@gmail.com>
2019-12-24 21:03:02 +0700

    👀

--- src/components/FirebaseConnect/FirebaseConnect.js
+++ src/components/FirebaseConnect/FirebaseConnect.js
@@ -35,6 +35,36 @@ export default function FirebaseConnect() {
     });
   }
 
+  const handleCreateUser = () => {
+    let newData = {
+      name: "Herby",
+      password: "herby",
+      username: "herby",
+      level: "admin"
+    }
+    db.collection("ellipsemobile").doc("102").set(newData)
+    .then(function() {
+        console.log("Add user successfully written!");
+    })
+    .catch(function(error) {
+        console.error("Error writing document: ", error);
+    });
+  }
+
+  const handleGetDocuments = () => {
+    db.collection("ellipsemobile")
+    .get()
+    .then(querySnapshot => {
+      const data = querySnapshot.docs.map(doc => {
+        return { id: doc.id, data: doc.data() }
+      });
+      console.log(data)
+    })
+    .catch(function(error) {
+      console.error("Error get collection: ", error);
+    });
+  }
+
   useEffect(() => {
     getCounter()
   }, [])
@@ -43,8 +73,12 @@ export default function FirebaseConnect() {
     <>
       <button onClick={handleAddCounter}>Add Counter</button>
       <hr />
+      <button onClick={handleCreateUser}>Add User</button>
+      <hr />
+      <button onClick={handleGetDocuments}>Get Documents</button>
+      <hr />
       <pre>
-        {JSON.stringify({ state: counters }, null, 2)}
+        { counters === undefined ? 'Loading..' : JSON.stringify(counters, null, 2)}
       </pre>
     </>
   )
