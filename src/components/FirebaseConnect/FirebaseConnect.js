acde64cc1399395972b3e6a6499fb9fa5837434a
ekaone <ekaone3033@gmail.com>
2019-12-24 13:27:01 +0700

    👀

--- src/components/FirebaseConnect/FirebaseConnect.js
+++ src/components/FirebaseConnect/FirebaseConnect.js
@@ -1,58 +1,52 @@
 import React, { useState, useEffect } from 'react'
 import { db } from './FirebaseConfig'
 
-
 export default function FirebaseConnect() {  
 
-  const [counters, setCounters] = useState(0)
-  const [dates, setDates] = useState(null)
-  const [collections, setcollections] = useState(null)
-
-  const handleAddDate = () => {
-    db.collection("mydates").doc("dates").set({
-      mydatevalue: dates,
-    })
-    .then(function() {
-        console.log("Date successfully written!");
-    })
-    .catch(function(error) {
-        console.error("Error writing document: ", error);
-    });
-  }
+  const [counters, setCounters] = useState()
 
   const handleAddCounter = () => {
-    db.collection("mycounter").doc("counter").set({
-      mycounter: counters,
-    })
+    let c = counters + 1
+    let newData = {
+      date: new Date(),
+      value: c,
+    }
+    db.collection("mycounter").doc("counter").set(newData)
     .then(function() {
-        console.log("Date successfully written!");
+        console.log("Counter successfully written!");
+        setCounters(c)
     })
     .catch(function(error) {
         console.error("Error writing document: ", error);
     });
   }
 
-  const getCollections = () => {
-    db.collection("mycounter")
+
+  const getCounter = () => {
+    db.collection('mycounter')
     .get()
     .then(querySnapshot => {
       const data = querySnapshot.docs.map(doc => doc.data());
-      // console.log(data); // array of cities objects
-      setcollections(data)
+      let counter = parseInt(data.map(d => d.value))
+      setCounters(counter)
+    })
+    .catch(function(error) {
+      console.error("Error get collection: ", error);
     });
   }
 
-  getCollections()
-
+  useEffect(() => {
+    getCounter()
+  }, [])
+  
   return (
     <>
-      Firebase
-      <br />
-      <button onClick={() => handleAddCounter(setCounters(ps => ps + 1))}>Counter</button>
-      <br />
-      <button onClick={() => handleAddDate(setDates(new Date()))}>Date</button>
-      <br />
-      <pre>{ JSON.stringify(collections, null, 2) }</pre>
+      <button onClick={handleAddCounter}>Add Counter</button>
+      <hr />
+      <pre>
+        {JSON.stringify({ state: counters }, null, 2)}
+      </pre>
     </>
   )
 }
+
