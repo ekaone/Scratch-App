ef50b37a8d0ab0c6bc471404472081f8acfd5f15
Eka Prasetia <ekaone3033@gmail.com>
2020-06-04 17:22:58 +0700

    Updates

---
+++ precache-manifest.d4ef4c061e6ceae52e2ad0c5613cf468.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4e8547017ae3e6f177254f4eda61d60",
+    "url": "/Scratch-App/index.html"
+  },
+  {
+    "revision": "cc54ce83cb6b05b2615f",
+    "url": "/Scratch-App/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "cc54ce83cb6b05b2615f",
+    "url": "/Scratch-App/static/js/2.58540502.chunk.js"
+  },
+  {
+    "revision": "3ab6e35b9324278604999bc7477ba74d",
+    "url": "/Scratch-App/static/js/2.58540502.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b59e9dc0f5d142a86f8",
+    "url": "/Scratch-App/static/js/main.6d02484d.chunk.js"
+  },
+  {
+    "revision": "db7198986a19de428087",
+    "url": "/Scratch-App/static/js/runtime-main.2d358da0.js"
+  }
+]);
